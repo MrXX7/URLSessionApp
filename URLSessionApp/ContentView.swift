c22323220d42e7aa509cfe08ddd8e13a12c1d30b
Oncu <oohancan@gmail.com>
2023-10-16 14:55:41 +0300

    Initial Commit

---
+++ URLSessionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSessionApp
+//
+//  Created by Oncu Can on 16.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSessionApp/URLSessionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionAppApp.swift
+//  URLSessionApp
+//
+//  Created by Oncu Can on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
